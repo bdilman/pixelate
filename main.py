83f8fc7dbaff59fbfaa41df87fa34faa3582f251
bdilman <bdilman@jedsy.com>
2022-07-08 15:36:54 +0200

    new paramteres added

--- main.py
+++ main.py
@@ -32,14 +32,52 @@ def pixellize(**kwargs):
 
     # reduce image size
     superpixel_size = int(kwargs.get("superpixel_size", 10))
-    reduced_size = (img_size[0] // superpixel_size, img_size[1] // superpixel_size)
-    img = img.resize(reduced_size, Image.BICUBIC)
+    print(superpixel_size)
 
-    # resize to original shape to give pixelated look
-    img = img.resize(img_size, Image.BICUBIC)
 
+    #### B_edit:
+    
+    
+    ## resize image with given image size 
+    # 0: original
+    # 1: superpixel size defines output image size
+    # 2: user_defined image_size (x_dim, y_dim)
+        
+    define_new_size = int(kwargs.get("new_size"),0)
+    new_dim_x = int(kwargs.get("new_x_dim",32))
+    new_dim_y = int(kwargs.get("new_y_dim",32))
+    # print("___Pixelate___ : define_new_size param", define_new_size)
+    # print("___Pixelate___ : new_x_dim param", new_dim_x)
+    # print("___Pixelate___ : new_y_dim param", new_dim_y)
+
+    if define_new_size == 0:
+        print("___Pixelate___ : original size", img_size)
+        reduced_size = (img_size[0] // superpixel_size, img_size[1] // superpixel_size)
+        img = img.resize(reduced_size, Image.BICUBIC)
+        # resize to original shape to give pixelated look
+        img = img.resize(img_size, Image.BICUBIC)
+        # plot result
+        img.save(save)
+    elif define_new_size == 1:
+        reduced_size = (img_size[0] // superpixel_size, img_size[1] // superpixel_size)
+        print("___Pixelate___ : superpixel param defines size", reduced_size)    
+        img = img.resize(reduced_size, Image.BICUBIC)
+        img.save(save)
+    elif define_new_size == 2:
+        ## B, non-rectangular shapes work as well
+        print("___Pixelate___ : user defined img size", new_dim_x,new_dim_y )
+        #Function Call for resizing image.
+        # img_resized = photo2pixelart(image=img_name,i_size=(new_dim_x,new_dim_y),o_size=img_size)
+        #convert to small image
+        small_img=img.resize((new_dim_x,new_dim_y),Image.BILINEAR)
+
+        # plot result
+        small_img.save(save)
+        
+    #### B_end edit    
+    
     # plot result
-    img.save(save)
+    # img.save(save)
 
 if __name__ == "__main__":
 
@@ -51,6 +89,11 @@ if __name__ == "__main__":
     parser.add_argument('--superpixel_size', default=2, required=True, nargs="?")
     parser.add_argument('--saturation', default=1.5 , required=True, nargs="?")
     parser.add_argument('--contrast', default=1.7 , required=True, nargs="?")
+    parser.add_argument('--new_size', default=0 , required=True, nargs="?")
+    parser.add_argument('--new_x_dim', default=32 , required=True, nargs="?")
+    parser.add_argument('--new_y_dim', default=32 , required=True, nargs="?")
+
+
     kwargs = vars(parser.parse_args())
 
     print( type(kwargs))
